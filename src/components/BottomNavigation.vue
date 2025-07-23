2bf15c2878cf6717b9bcd371668f465a02f7a85a
WuFeng1861 <71131991+WuFeng1861@users.noreply.github.com>
2025-07-24 01:58:39 +0800

    更换节点的SVG图标

--- src/components/BottomNavigation.vue
+++ src/components/BottomNavigation.vue
@@ -66,7 +66,7 @@ const navigateTo = (path: string) => {
         :class="currentRoute === '/node' ? 'text-alpha-primary' : 'text-gray-400 hover:text-gray-200'"
       >
         <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
         </svg>
         <span class="text-xs mt-1">{{ t('nav.node') }}</span>
       </button>
